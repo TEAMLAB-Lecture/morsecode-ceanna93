cf6b2ecebfdfe7ee62513c24142765f54008a31c
ceanna93 <fairyanna@naver.com>
2021-01-21 16:48:18 +0900

    Update my code

--- morsecode.py
+++ morsecode.py
@@ -149,10 +149,12 @@ def get_cleaned_english_sentence(raw_english_sentence):
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당 또는 필요에 따라 자유로운 수정
     result = ""
+    sign = [ ".", ",", "!", "?"]
     text = raw_english_sentence
     for c in text:
-        if c.isalpha() or c == ' ':
-            result += c
+        if c in sign:
+            continue
+        result += c
     result = ' '.join(result.split())
 
     return result
